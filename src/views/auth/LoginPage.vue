a0d87fa5050aa4439ccd6df194d1b3782bc3db33
Yoga Mileniandi <mileniandi39@gmail.com>
2025-04-14 13:05:51 +0700

    fix: login and register handling error

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -1,58 +1,62 @@
-<script setup>
+<script setup lang="ts">
+import useForm from '@/composables/useForm';
 import { login } from '@/services/AuthService';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const email = ref('');
-const password = ref('');
-const is_remember = ref(false);
-const is_loading = ref(false);
+const { form, errors, submitForm, isLoading } = useForm({
+    email: '',
+    password: '',
+})
 
 const handleLogin = async () => {
-    try{
-        is_loading.value = true
-        await login(email.value, password.value)
-        router.push('/')
-    }catch (error){
-        console.warn(error)
-    }finally {
-        is_loading.value = false
-    }
-	
+    await login(form.email, form.password)
+    router.push('/')
 }
 
 </script>
 <template>
-	 <div class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
+    <div
+        class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden">
         <div class="flex flex-col items-center justify-center">
             <div class="p-2 rounded-4xl">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="flex flex-col justify-center items-center">
-						<img src="/logo.png" alt="logo" class="h-14 w-auto"/>                        
-                        <p class="text-muted-color font-medium my-6">Sign in to continue</p>           
-					</div>
-                    <div>
-						<FloatLabel variant="in" class="mb-8">
-							<InputText id="email" type="text" class="w-full md:w-[30rem]" v-model="email" />
-							<label for="email">Email</label>
-						</FloatLabel>
-						<FloatLabel variant="in" class="mb-8">
-							<Password id="password" v-model="password" :toggleMask="true" class="mb-4" fluid :feedback="false"></Password>
-							<label for="password">Password</label>
-						</FloatLabel>
+                        <img src="/logo.png" alt="logo" class="h-14 w-auto" />
+                        <p class="text-muted-color font-medium my-6">Sign in to continue</p>
+                    </div>
+                    <form @submit.prevent="submitForm(handleLogin)">
+                        <IftaLabel>
+                            <InputText
+                                v-model="form.email"
+                                :invalid="!!errors?.email"
+                                class="w-[30rem]"
+                            />
+                            <IErrorMessage :value="errors?.email" />
+                            <label>Email</label>
+                        </IftaLabel>
+                        <IftaLabel class="mt-8">
+                            <Password id="password" v-model="form.password" :invalid="!!errors?.password" :toggleMask="true" class="mb-4 w-[30rem]" fluid
+                                :feedback="false"></Password>
+                            <IErrorMessage :value="errors?.password" />
+                            <label>Password</label>
+                        </IftaLabel>
 
                         <div class="flex items-center justify-between mt-2 mb-8 gap-8">
                             <div class="flex items-center">
-                                <Checkbox v-model="is_remember" id="rememberme" binary class="mr-2"></Checkbox>
-                                <label for="rememberme">Remember me</label>
+                                <!-- <Checkbox v-model="is_remember" id="rememberme" binary class="mr-2"></Checkbox>
+                                <label for="rememberme">Remember me</label> -->
                             </div>
-                            <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary" @click="router.push('/auth/forgot-password')">Forgot password?</span>
+                            <span class="font-medium no-underline ml-2 text-right cursor-pointer text-primary"
+                                @click="router.push('/auth/forgot-password')">Forgot password?</span>
                         </div>
-                        <Button label="Sign In" class="w-full" @click="handleLogin"></Button>
-                        <p class="text-center mt-4">Don't have an account? <span class="text-primary cursor-pointer" @click="router.push('/auth/register')">Register</span></p>
-                    </div>
+                        <IButton type="submit" label="Login" size="large" class="w-full"
+                            :loading="isLoading" />
+                        <p class="text-center mt-4">Don't have an account? <span class="text-primary cursor-pointer"
+                                @click="router.push('/auth/register')">Register</span></p>
+                    </form>
                 </div>
             </div>
         </div>
--- src/views/auth/RegisterPage.vue
+++ src/views/auth/RegisterPage.vue
@@ -1,26 +1,20 @@
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import useForm from '@/composables/useForm';
 import { register } from '@/services/AuthService';
+import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
-const name = ref('');
-const email = ref('');
-const password = ref('');
-const passwordConfirmation = ref('');
-const is_loading = ref(false);
+const { form, errors, submitForm, isLoading } = useForm({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirmation: '',
+});
 
 const handleRegister = async () => {
-    try {
-        is_loading.value = true;
-        await register(name.value, email.value, password.value, passwordConfirmation.value);
-        router.push('/auth/login');
-    } catch (error) {
-        console.warn(error);
-    } finally {
-        is_loading.value = false;
-    }
+    await register(form.name, form.email, form.password, form.passwordConfirmation);
+    router.push('/auth/login');
 };
 </script>
 
@@ -30,31 +24,66 @@ const handleRegister = async () => {
             <div class="p-2 rounded-4xl">
                 <div class="w-full bg-surface-0 dark:bg-surface-900 py-20 px-8 sm:px-20" style="border-radius: 53px">
                     <div class="flex flex-col justify-center items-center">
-                        <img src="/logo.png" alt="logo" class="h-14 w-auto"/>
+                        <img src="/logo.png" alt="logo" class="h-14 w-auto" />
                         <p class="text-muted-color font-medium my-6">Create an account</p>
                     </div>
-                    <div>
-                        <FloatLabel variant="in" class="mb-8">
-                            <InputText id="name" type="text" class="w-full md:w-[30rem]" v-model="name" />
-                            <label for="name">Name</label>
-                        </FloatLabel>
-                        <FloatLabel variant="in" class="mb-8">
-                            <InputText id="email" type="text" class="w-full md:w-[30rem]" v-model="email" />
-                            <label for="email">Email</label>
-                        </FloatLabel>
-                        <FloatLabel variant="in" class="mb-8">
-                            <Password id="password" v-model="password" :toggleMask="true" class="mb-4" fluid :feedback="false"></Password>
-                            <label for="password">Password</label>
-                        </FloatLabel>
-                        <FloatLabel variant="in" class="mb-8">
-                            <Password id="passwordConfirmation" v-model="passwordConfirmation" :toggleMask="true" class="mb-4" fluid :feedback="false"></Password>
-                            <label for="passwordConfirmation">Confirm Password</label>
-                        </FloatLabel>
-                        <Button label="Register" class="w-full" @click="handleRegister"></Button>
-                        <p class="text-center mt-4">Already have an account? <span class="text-primary cursor-pointer" @click="router.push('/auth/login')">Login</span></p>
-                    </div>
+                    <form @submit.prevent="submitForm(handleRegister)">
+                        <IftaLabel>
+                            <InputText
+                                v-model="form.name"
+                                :invalid="!!errors?.name"
+                                class="w-[30rem]"
+                            />
+                            <IErrorMessage :value="errors?.name" />
+                            <label>Name</label>
+                        </IftaLabel>
+                        <IftaLabel class="mt-8">
+                            <InputText
+                                v-model="form.email"
+                                :invalid="!!errors?.email"
+                                class="w-[30rem]"
+                            />
+                            <IErrorMessage :value="errors?.email" />
+                            <label>Email</label>
+                        </IftaLabel>
+                        <IftaLabel class="mt-8">
+                            <Password
+                                v-model="form.password"
+                                :invalid="!!errors?.password"
+                                :toggleMask="true"
+                                class="mb-4 w-[30rem]"
+                                fluid
+                                :feedback="false"
+                            />
+                            <IErrorMessage :value="errors?.password" />
+                            <label>Password</label>
+                        </IftaLabel>
+                        <IftaLabel class="mt-8">
+                            <Password
+                                v-model="form.passwordConfirmation"
+                                :invalid="!!errors?.passwordConfirmation"
+                                :toggleMask="true"
+                                class="mb-4 w-[30rem]"
+                                fluid
+                                :feedback="false"
+                            />
+                            <IErrorMessage :value="errors?.passwordConfirmation" />
+                            <label>Confirm Password</label>
+                        </IftaLabel>
+                        <IButton
+                            type="submit"
+                            label="Register"
+                            size="large"
+                            class="w-full"
+                            :loading="isLoading"
+                        />
+                        <p class="text-center mt-4">
+                            Already have an account? 
+                            <span class="text-primary cursor-pointer" @click="router.push('/auth/login')">Login</span>
+                        </p>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
-</template> 
+</template>
